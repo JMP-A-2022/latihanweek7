7c2e729a3fda65c8a03a582a64c7fa9f1bc9e814
Laita Zidan <2131710100@student.polinema.ac.id>
2022-08-12 01:58:46 +0700

    commit fro laita zidan

--- dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/ResetPasswordActivity.java
+++
@@ -1,9 +0,0 @@
-package polinema.ac.id.dtschapter03_starter;
-
-import android.app.Activity;
-import android.view.View;
-
-public class ResetPasswordActivity extends Activity {
-    public void postChangePassword(View view) {
-    }
-}
